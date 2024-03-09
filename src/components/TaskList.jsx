721afab0b43faf33f7246cf3fa7e940527cb6647
Catalin <catalin.mandru@stud.ubbcluj.ro>
2024-03-09 17:01:11 +0200

    Functionality complete

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,9 +2,13 @@ import Task from "./Task";
 import styles from "./taskList.module.css";
 
 export default function TaskList({ todoList, setTodoList }) {
+  const sortedTasks = todoList
+    .slice()
+    .sort((a, b) => Number(a.complete) - Number(b.complete));
+
   return (
     <div className={styles.list}>
-      {todoList.map((item) => (
+      {sortedTasks.map((item) => (
         <Task
           key={item.name}
           item={item}
